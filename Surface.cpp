2054d861628598702354c5fc483f522933980286
Federico Ferri <federico.ferri.it@gmail.com>
2015-03-11 11:06:49 +0100

    correct args to drawTriangle

--- Surface.cpp
+++ Surface.cpp
@@ -330,7 +330,7 @@ void Surface::drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, SDL_C
         }
     }
 
-    drawTriangleNoLock(x1, y1, x2, y2, rawColor(color));
+    drawTriangleNoLock(x1, y1, x2, y2, x3, y3, rawColor(color));
 
     if(SDL_MUSTLOCK(surface))
     {
