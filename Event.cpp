1e0982ca92fb8a2ff61550bb7cf66e071d8f4945
Federico Ferri <federico.ferri.it@gmail.com>
2015-03-11 11:55:16 +0100

    implement the Event class

--- Event.cpp
+++ Event.cpp
@@ -1,1 +1,43 @@
 #include "Event.h"
+
+Event::Event()
+{
+}
+
+Event::~Event()
+{
+}
+
+bool Event::operator<(const Event& rhs) const
+{
+    return stamp < rhs.stamp;
+}
+
+size_t Event::getLength() const
+{
+    return data.size();
+}
+
+char Event::getType(size_t index) const
+{
+    return data[index].type;
+}
+
+void Event::getType(char *type) const
+{
+    for(std::vector<EventData>::const_iterator it = data.begin(); it != data.end(); ++it)
+    {
+        *type++ = it->type;
+    }
+}
+
+void Event::add(const EventData& e)
+{
+    data.push_back(e);
+}
+
+void Event::remove(size_t index)
+{
+    data.erase(data.begin() + index);
+}
+
--- Event.h
+++ Event.h
@@ -1,13 +1,43 @@
 #ifndef EVENT_H_INCLUDED
 #define EVENT_H_INCLUDED
 
+#include <cstddef>
+#include <vector>
+
+struct EventDataMIDI
+{
+    unsigned char port_id;
+    unsigned char status;
+    unsigned char data1;
+    unsigned char data2;
+};
+
+struct EventData
+{
+    char type;
+    union {
+        int i;
+        float f;
+        EventDataMIDI m;
+    } data;
+};
+
 class Event
 {
+protected:
+    unsigned long stamp;
+    std::vector<EventData> data;
+
 public:
-    unsigned int floatDataSize;
-    float *floatData;
-    unsigned int intDataSize;
-    int *intData;
+    Event();
+    virtual ~Event();
+    bool operator<(const Event& rhs) const;
+
+    size_t getLength() const;
+    char getType(size_t index) const;
+    void getType(char *type) const;
+    void add(const EventData& e);
+    void remove(size_t index);
 };
 
 #endif // EVENT_H_INCLUDED
