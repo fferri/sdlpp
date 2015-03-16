f710690e86515994bb99f1137c6886756e5aac18
Federico Ferri <federico.ferri.it@gmail.com>
2015-03-17 00:40:16 +0100

    logging in colors

--- Logger.h
+++ Logger.h
@@ -14,15 +14,15 @@ enum LOG_LEVEL
 };
 
 #define LOG_LEVEL_STR(l) \
-    ((l) == FATAL ? "FATAL" : \
-    ((l) == ERROR ? "ERROR" : \
-    ((l) == WARN  ? "WARN " : \
-    ((l) == INFO  ? "INFO " : \
-    ((l) == DEBUG ? "DEBUG" : \
-    ((l) == TRACE ? "TRACE" : "<<<UNKNOWN LOGGER LEVEL>>>"))))))
+    ((l) == FATAL ? "\033[30;7;31m" : \
+    ((l) == ERROR ? "\033[1;31m" : \
+    ((l) == WARN  ? "\033[1;33m" : \
+    ((l) == INFO  ? "\033[1;37m" : \
+    ((l) == DEBUG ? "\033[37m" : \
+    ((l) == TRACE ? "\033[30m" : ""))))))
 
 extern LOG_LEVEL log_level;
 
-#define LOG(l) if((l) < log_level) ; else std::cout << "[" << LOG_LEVEL_STR(l) << "] " << __FILE__ << ":" << __LINE__ << " - " << __func__ << ": "
+#define LOG(l) if((l) < log_level) ; else std::cout << LOG_LEVEL_STR(l) << __FILE__ << ":" << __LINE__ << " - " << __func__ << ":\033[0m "
 
 #endif // LOGGER_H_INCLUDED
